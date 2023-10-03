8d6715b193fb760892c4ac8e2576c3a93cbbb303
Levy Sampaio <levyaraujo42@gmail.com>
2023-10-03 13:04:32 -0300

    modules engine

--- tools/modularizer/modularizer.go
+++ tools/modularizer/modularizer.go
@@ -11,22 +11,23 @@ import (
 )
 
 type Modularizer struct {
-	ModulesList map[string]string
-	Logger      *logger.Logger
+	Logger *logger.Logger
 }
 
+var ModulesList = map[string]string{
+	"grpc": "https://github.com/not-empty/zord-grpc-microframework-golang.git",
+}
+var IgnoreList []string
+
 func NewModularizer(l *logger.Logger) *Modularizer {
-	modulesList := map[string]string{
-		"grpc": "https://github.com/not-empty/zord-microframework-golang.git",
-	}
+	IgnoreList = append(IgnoreList, ".git", "application")
 	return &Modularizer{
-		ModulesList: modulesList,
-		Logger:      l,
+		Logger: l,
 	}
 }
 
 func (m Modularizer) GetModule(module string) error {
-	module, exists := m.ModulesList[module]
+	module, exists := ModulesList[module]
 	if !exists {
 		err := errors.New("modulo inexistente ou não mapeado")
 		m.Logger.Error(err)
@@ -41,9 +42,22 @@ func (m Modularizer) GetModule(module string) error {
 	}
 
 	filepath.Walk("tools/modularizer/temp", func(path string, info fs.FileInfo, err error) error {
+		if m.isInIgnore(info.Name()) {
+			return filepath.SkipDir
+		}
 		if info.IsDir() {
-			m.createFolder(path)
+			name := m.getFullFilePath(path)
+			if name == "" {
+				return nil
+			}
+			m.createFolder(name)
+			m.Logger.Info(path)
 			return nil
+		}
+		err = m.createFile(path, m.getFullFilePath(path), make(map[string]string))
+		if err != nil {
+			m.Logger.Error(err)
+			return err
 		}
 		return nil
 	})
@@ -65,6 +79,48 @@ func (m *Modularizer) replacer(str string, replaces map[string]string) string {
 
 func (m *Modularizer) createFolder(path string) {
 	if _, err := os.Stat(path); os.IsNotExist(err) {
-		os.Mkdir(path, 0755)
+		err = os.Mkdir(path, 0755)
+		if err != nil {
+			m.Logger.Error(err)
+		}
 	}
+}
+
+func (m *Modularizer) createFile(from string, to string, replacers map[string]string) error {
+	data := m.getFileData(from)
+	domainContent := m.replacer(data, replacers)
+
+	err := os.WriteFile(to, []byte(domainContent), 0755)
+	if err != nil {
+		m.Logger.Error(err)
+		return err
+	}
+	m.Logger.Info("CREATE FILE: " + to)
+	return nil
+}
+
+func (m *Modularizer) getFileData(path string) string {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		m.Logger.Error(err)
+	}
+	return string(data)
+}
+
+func (m *Modularizer) getFullFilePath(path string) string {
+	pathArray := strings.Split(path, "/temp/")
+	if len(pathArray) > 1 {
+		return pathArray[1]
+	}
+	return ""
+}
+
+func (m *Modularizer) isInIgnore(file string) bool {
+	for _, ignore := range IgnoreList {
+		if file == ignore {
+
+			return true
+		}
+	}
+	return false
 }

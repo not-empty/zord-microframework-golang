62c700e95ca4e2fb6a1ad8153b211c65aa9ae5e7
Samuel da Silva <samuel.silva@1doc.com.br>
2023-10-18 20:34:34 -0300

    chore: modify arch config

--- tools/generator/generator.go
+++ tools/generator/generator.go
@@ -1,1 +1,29 @@
 package generator
+
+import (
+	"go-skeleton/application/services"
+
+	"github.com/spf13/cobra"
+)
+
+type CodeGenerator struct {
+	Logger services.Logger
+}
+
+func NewCodeGenerator(l services.Logger) *CodeGenerator {
+	return &CodeGenerator{
+		Logger: l,
+	}
+}
+
+func (g *CodeGenerator) Handler(cmd *cobra.Command) {
+	g.getFlags(cmd)
+}
+
+func (g *CodeGenerator) getFlags(cmd *cobra.Command) {
+	cmd.Flags().GetBool()
+}
+
+func (g *CodeGenerator) isInvalidDomain(domain string) bool {
+	return domain == ""
+}

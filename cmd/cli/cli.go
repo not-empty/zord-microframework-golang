1b6ba43abcd1d4a5bd10d21361c459c92e1fa6af
Levy Sampaio <levyaraujo42@gmail.com>
2023-10-22 16:27:58 -0300

    chore: improve cli commands logics and decrase complexity

--- cmd/cli/cli.go
+++ cmd/cli/cli.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"go-skeleton/pkg"
-	"go-skeleton/pkg/database"
-	"go-skeleton/pkg/logger"
-	"go-skeleton/tools/generator"
-	"go-skeleton/tools/migrator"
-
 	"github.com/spf13/cobra"
+	"go-skeleton/cmd/cli/generator"
+	"go-skeleton/cmd/cli/migrator"
+	"go-skeleton/pkg"
 )
-
-var domain string
 
 type Cli struct {
 	Environment string
@@ -34,69 +28,8 @@ func main() {
 }
 
 func (c *Cli) Start() {
-	c.initFlags(c.cmd)
-	createDomain := &cobra.Command{
-		Use:    "create-domain",
-		Short:  "Create a new domain service",
-		PreRun: c.BootGenerator,
-		Run:    c.CreateDomain,
-	}
-	createDomain.Flags().BoolVarP(&c.validator, "validator", "v", false, "Create domain with validation")
-	c.cmd.AddCommand(
-		createDomain,
-		&cobra.Command{
-			Use:    "destroy-domain",
-			Short:  "Destroy a domain service",
-			PreRun: c.BootGenerator,
-			Run:    c.DestroyDomain,
-		},
-		&cobra.Command{
-			Use:    "migrate",
-			Short:  "Migrate Gorm Database",
-			PreRun: c.BootMigrator,
-			Run:    c.Migrate,
-		},
-	)
-}
-
-func (c *Cli) CreateDomain(_ *cobra.Command, args []string) {
-	generatorInstance := generator.NewGenerator(pkg.CliDependencies["logger"].(*logger.Logger))
-	if len(args) > 0 {
-		domain = args[0]
-	}
-	err := generatorInstance.CreateDomain(domain, c.validator)
-	pkg.Logger.Error(err)
-}
-
-func (c *Cli) DestroyDomain(_ *cobra.Command, args []string) {
-	generatorInstance := generator.NewGenerator(pkg.CliDependencies["logger"].(*logger.Logger))
-	if len(args) > 0 {
-		domain = args[0]
-	}
-	err := generatorInstance.DestroyDomain(domain)
-	pkg.Logger.Error(err)
-}
-
-func (c *Cli) Migrate(_ *cobra.Command, _ []string) {
-	migratorInstance := migrator.NewMigrator(pkg.MigratorDependencies["mysql"].(*database.MySql))
-	migratorInstance.MigrateAllDomains()
-}
-
-func (c *Cli) initFlags(cmd *cobra.Command) {
-	cmd.PersistentFlags().StringVar(&domain, "domain", "", "Describe name to New Domain")
-	cmd.MarkFlagsMutuallyExclusive("domain")
-}
-
-func (c *Cli) BootGenerator(_ *cobra.Command, _ []string) {
-	for index, dep := range pkg.CliDependencies {
-		dep.Boot()
-		pkg.Logger.Info(fmt.Sprintf("[Kernel.Kernel] Booting %s", index))
-	}
-}
-
-func (c *Cli) BootMigrator(_ *cobra.Command, _ []string) {
-	for index, dep := range pkg.MigratorDependencies {
-		dep.Boot()
-		pkg.Logger.Info(fmt.Sprintf("[Kernel.Kernel] Booting %s", index))
-	}
+	generatorInstance := generator.NewGenerator()
+	generatorInstance.DeclareCommand(c.cmd)
+	migratorInstance := migrator.NewMigrator()
+	migratorInstance.DeclareCommands(c.cmd)
 }
